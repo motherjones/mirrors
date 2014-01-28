92a9e5c84042cf8edf6afafe410a4c9360c6eae2
Jon Friedman <jfriedman@motherjones.com>
2014-01-27 17:18:02 -0800

    added urls for content app

---
+++ content/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import patterns, url
+
+urlpatterns = patterns(
+    'content.views',
+    url(r'^(?P<slug>[-\w]+)$', 'get_content'),
+    url(r'^(?P<slug>[-\w]+)/data$', 'get_content_data'),
+    url(r'^(?P<slug>[-\w]+)/revision/(?P<revision>\d+)$',
+        'get_content_revision'),
+    url(r'^(?P<slug>[-\w]+)/revision/(?P<revision>\d+)/data$',
+        'get_content_revision_data'),
+)
