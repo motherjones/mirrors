dea31f04595cebbb80c044ff197b76e6e589c957
Jon Friedman <jfriedman@motherjones.com>
2014-04-03 14:46:38 -0700

    WEB-339 serializer for Components done-ish

--- mirrors/serializers.py
+++ mirrors/serializers.py
@@ -1,25 +1,33 @@
 from rest_framework import serializers
-from mirrors.models import Component, ComponentRevision
-
-
-class ChildComponentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Component
-        fields = ('slug', 'metadata', 'content_type', 'publish_date',
-                  'schema_name', 'members')
+from mirrors.models import Component, ComponentAttribute, ComponentRevision
 
 
 class ComponentSerializer(serializers.ModelSerializer):
-    attributes = serializers.RelatedField(many=True)
-    members = serializers.RelatedField(many=True)
     revisions = serializers.RelatedField(many=True)
+    data_uri = serializers.URLField(read_only=True)
+    attributes = serializers.SerializerMethodField('_get_attributes')
 
     class Meta:
         model = Component
         fields = ('slug', 'metadata', 'content_type', 'publish_date',
-                  'schema_name', 'members', 'revisions')
+                  'schema_name', 'revisions', 'data_uri', 'attributes')
 
-ComponentSerializer.base_fields['members'] = ComponentSerializer()
+    def _get_attributes(self, obj):
+        result = []
+        attribute_names = [o.name for o in obj.attributes.distinct('name')]
+
+        for n in attribute_names:
+            attr = obj.get_attribute(n)
+
+            if isinstance(attr, list):
+                result.append({
+                    'name': n, 
+                    'value':[ComponentSerializer(a).data for a in attr]
+                })
+            else:
+                result.append(ComponentSerializer(attr).data)
+
+        return result
 
 
 class ComponentRevisionSerializer(serializers.ModelSerializer):
