a795eef0f53d4d5ea9a9a6fa014f23584ecd993e
Jon Friedman <jfriedman@motherjones.com>
2014-06-27 11:13:59 -0700

    merged RC1/master into RC1/component-lock

---
+++ mirrors/tests/test_auth.py
@@ -0,0 +1,44 @@
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+
+class ComponentAuthenticationTest(APITestCase):
+    fixtures = ['users.json', 'component_data.json']
+
+    def test_noauthed_rejects(self):
+        url = reverse('component-detail', kwargs={
+            'slug': 'component-with-svg-data'
+        })
+
+        res = self.client.get(url)
+        self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_authed_as_user_accepts(self):
+        url = reverse('component-detail', kwargs={
+            'slug': 'component-with-svg-data'
+        })
+
+        self.client.login(username='test_user', password='password1')
+        res = self.client.get(url)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_authed_as_staff_accepts(self):
+        url = reverse('component-detail', kwargs={
+            'slug': 'component-with-svg-data'
+        })
+
+        self.client.login(username='test_staff', password='password1')
+        res = self.client.get(url)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_authed_as_admin_accepts(self):
+        url = reverse('component-detail', kwargs={
+            'slug': 'component-with-svg-data'
+        })
+
+        self.client.login(username='test_admin', password='password1')
+        res = self.client.get(url)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
