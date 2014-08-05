c622b8625305941f53b8606aa648ab250d88e4d5
Jon Friedman <jfriedman@motherjones.com>
2014-08-05 12:10:47 -0700

    added a few dependencies to setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,10 @@ setup(name='mirrors',
     version=__import__('mirrors').__version__,
     packages=find_packages(exclude=['sample_project']),
     install_requires=[
-        'South>=0.8.4',
         'jsonfield>=0.9.20',
-        'djangorestframework>=2.3.12'
+        'psycopg2>=2.5.1',
+        'djangorestframework>=2.3.13',
+        'jsonschema>=2.3.0',
     ],
     extras_require = {
         'development': ['Sphinx>=1.2.1', 'coverage'],
